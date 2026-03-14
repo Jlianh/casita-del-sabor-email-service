1fafda725871c1e305fb33f5493ed65fa8480022
Julian <jscristancho83@gmail.com>
2026-03-13 20:49:34 -0500

    cors al servicio

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const quotationRoutes = require('./quotation');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({ status: 'ok', message: 'Spice Quotation API running' });
